6b8d60ecdabb5353a110f27a5cfe05bd0ea4ba71
github_username <you@domain.com>
2023-05-29 03:51:38 +0600

    jwt token validation

--- index.js
+++ index.js
@@ -10,6 +10,23 @@ const port  = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+const verifyJWT = (req, res, next) =>{
+  const authorization = req.headers.authorization;
+  if(!authorization){
+    return res.status(401).send({error: true, message:'unauthorization'})
+  }
+  // bearer token
+  const token = authorization.split('')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({error: true, message:'unauthorization'})
+    }
+    req.decoded = decoded;;
+    next();
+  })
+}
+
 // mongodb
 
 
@@ -38,11 +55,11 @@ async function run() {
     const cartCollection = client.db("bistroDB").collection("carts");
 
 
-    app.post('/jwt', (req, res) =>{
+    app.post('/jwt',  (req, res) =>{
       const user = req.body;
-      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn:'1h'})
       res.send({token});
-    } )
+    })
 
 
     // users related apis
@@ -89,12 +106,18 @@ async function run() {
     })
 
     //  cart collection apis
-    app.get('/carts', async(req, res) =>{
+    app.get('/carts', verifyJWT,async(req, res) =>{
       const email = req.query.email;
       console.log(email);
       if(!email){
         res.send([]);
       }
+
+      const deecodedEmail = req.decoded.email;
+      if(email !== deecodedEmail){
+        return res.status(403).send({error: true, message:'probidden access'})
+      }
+
       const query = {email:email};
       const result = await cartCollection.find(query).toArray();
       res.send(result);
