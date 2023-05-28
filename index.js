ccd0855b0b3587e1d0300ee505418935eb17eb2d
github_username <you@domain.com>
2023-05-29 02:45:58 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express  = require('express');
 const app  = express();
 require('dotenv').config()
 const cors  = require('cors')
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port  = process.env.PORT || 5000;
 
@@ -36,10 +37,40 @@ async function run() {
     const reviewCollection = client.db("bistroDB").collection("reviews");
     const cartCollection = client.db("bistroDB").collection("carts");
 
+
+    app.post('/jwt', (req, res) =>{
+      const user = req.body;
+      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET,{expiresIn:'1h'})
+      res.send({token});
+    } )
+
+
     // users related apis
+    app.get('/users', async (req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
     app.post('/users', async(req,res) =>{
       const user = req.body;
+      console.log(user);
+      const query = {email : user.email}
+      const existingUser = await usersCollection.findOne(query);
+      if(existingUser){
+        return res.send({message: 'user already exists'})
+      }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    })
+
+    app.patch('/users/admin/:id', async (req,res) =>{
+      const id = req.params.id;
+      const filter ={_id: new ObjectId(id)};
+      const updateDoc = {
+        $set: {
+          role:'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updateDoc)
       res.send(result);
     })
 
